f35760d0a78e80fa64630e6f246a47ba0ca3edc0
carlos calderon <cdcalderon@gmail.com>
2018-10-20 21:15:16 -0500

    add initial delete movie

--- src/components/movies.jsx
+++ src/components/movies.jsx
@@ -5,24 +5,38 @@ class Movies extends Component {
   state = {
     movies: getMovies()
   };
+
+  handleDelete = movie => {
+    console.log(movie);
+  };
+
   render() {
     return (
       <table className="table">
         <thead>
           <tr>
-            <td>Title</td>
-            <td>Genre</td>
-            <td>Stock</td>
-            <td>Rate</td>
+            <th>Title</th>
+            <th>Genre</th>
+            <th>Stock</th>
+            <th>Rate</th>
+            <th />
           </tr>
         </thead>
         <tbody>
           {this.state.movies.map(movie => (
-            <tr>
+            <tr key={movie._id}>
               <td>{movie.title}</td>
               <td>{movie.genre.name}</td>
               <td>{movie.numberInStock}</td>
               <td>{movie.dailyRentalRate}</td>
+              <td>
+                <button
+                  onClick={() => this.handleDelete(movie)}
+                  className="btn btn-danger btn-sm"
+                >
+                  Delete{" "}
+                </button>
+              </td>
             </tr>
           ))}
         </tbody>
